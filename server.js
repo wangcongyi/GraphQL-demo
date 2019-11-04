49318a1134601e017158356c54bb61b4b7d77acf
王聪奕 <360805958@qq.com>
2019-11-04 14:27:49 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const schema = require('./schema/schema')
+
+const app = express()
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql:true
+}))
+
+
+app.listen(4000, () => {
+  console.log('server ok')
+})
