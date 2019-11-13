9213d1c3ce95ae3aab4ba16e3ae2b9286ac7fb64
王聪奕 <360805958@qq.com>
2019-11-13 10:13:31 +0800

    Update server.js

--- server.js
+++ server.js
@@ -13,3 +13,24 @@ app.use('/graphql', graphqlHTTP({
 app.listen(4000, () => {
   console.log('server ok')
 })
+
+///////////
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const schema = require('./schema/schema')
+const mongoose = require('mongoose')
+
+const app = express()
+
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true })
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+}))
+
+
+app.listen(4000, () => {
+  console.log('server ok')
+})
+
